109566557603e8aa8ca1af7c4774dc9ea078f70d
Amartuvshin <Amartuvshin@MacBooks-MacBook-Pro.local>
2020-07-20 14:53:41 +0800

    commitment

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,16 +11,17 @@ export const Login = () => {
                     <IconEndBracket />
                 </div>
                 <div className='font-lobster c-primary fs-40 lh-44 flex-center'>
-                    Boginoo
+                    Boginoo 
                 </div>
                 <div className='font-ubuntu c-primary fs-20 lh-44 flex-center'>
                 Нэвтрэх
                 </div>
                 <div className='mt-5 flex justify-around flex-colce'>
                     
-                    <Input className='h-5 ph-4' placeholder='https://www.web-huudas.mn' />
-
-                    <Input className='h-5 ph-4' placeholder='https://www.web-huudas.mn' />
+                    <div> E-mail</div>
+                    <Input className='h-5 ph-4' placeholder='E-mail' />
+                    <div> password</div>
+                    <Input className='h-5 ph-4' placeholder='password' />
                     <Button className="font-ubuntu lh-23 bold c-default h-5 ph-4 ml-3 b-primary">Богиносгох</Button>
                 </div>
             </div>
