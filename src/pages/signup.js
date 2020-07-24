8eb7c887ac69370c5aab6ef1573aed0236333809
Amartuvshin <Amr2vsn@Amr2vsn.local>
2020-07-24 19:57:02 +0800

    forminput

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Layout, Button, Input, IconDash, IconEndBracket, IconStartBracket } from '../components/';
+import React, { useContext, useState } from 'react';
+import { Layout, Button, FormInput, Input, IconDash, IconEndBracket, IconStartBracket } from '../components/';
 import {AuthContext} from '../providers/auth-user-provider';
 // import {useHistory} from 'react-router-dom';
 import {useFirebase} from '../firebase';
@@ -13,7 +13,7 @@ export const Signup = () => {
     const handleChangeUsername = (e) => setState ({...state, username: e.target.value});
     const handleChangeEmail = (e) => setState({...state, email: e.target});
     const handleChangePassword = (e) => setState({...state, password: e.target.value});
-    const handleChangePassword = (e) => setState({...state, password2: e.target.value});
+    const handleChangePassword2 = (e) => setState({...state, password2: e.target.value});
 
     const signUp = async() => {
         if (!(state.email && state.password && state.password2)) {
@@ -53,12 +53,12 @@ export const Signup = () => {
                 </div>
                 <div className='mt-5 flex justify-around flex-colce'>
                     
-                    <div className="font-ubuntu">E-mail</div>
-                    <Input className='h-5 ph-4' placeholder='E-mail' />
-                    <div className="font-ubuntu"> password</div>
-                    <Input className='h-5 ph-4' placeholder='password' />
-                    
-                    <Button className="font-ubuntu lh-23 bold c-default h-5 ph-4 ml-3 b-primary">Бүртгүүлэх</Button>
+                <FormInput label='Хэрэглэгчийн нэр' type='text' placeholder='username' className='h-5 w-8' value={state.username} onChange={handleChangeUsername}/>
+                    <FormInput label='Цахим хаяг' type='email' placeholder='name@mail.domain' className='h-5 w-8' value={state.email} onChange={handleChangeEmail}/>
+                    <FormInput label='Нууц үг' type='password' placeholder='password' className='h-5 w-8' value={state.password} onChange={handleChangePassword} />
+                    <FormInput label='Нууц үгээ давтна уу?' type='password' placeholder='password' className='h-5 w-8' value={state.password2} onChange={handleChangePassword2} />
+
+                    <Button className='font-ubuntu w-8 fs-20 lh-23 bold c-default h-5 ph-4 b-primary' onClick={signUp}>Бүртгүүлэх</Button>
                 </div>
             </div>
         </Layout>
